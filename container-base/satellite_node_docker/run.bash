bc6499d9b238b48cc8e0c47cd6e8fd18c38b6fc1
realssd <luwenhao_2001@outlook.com>
2023-10-22 00:13:59 +0800

    [refactor]: 修改卫星容器启动脚本名

--- container-base/satellite_node_docker/run.bash
+++
@@ -1,4 +0,0 @@
-# cd /edit && nohup python3 start_compress.py > compress_log.log 2>&1 &
-trap "exit 0" TERM
-cd /satellite_node/ && ./bootstrap.sh
-
---
+++ container-base/satellite_node_docker/start.sh
@@ -0,0 +1,2 @@
+cd /satellite_node/ && ./bootstrap.sh
+
