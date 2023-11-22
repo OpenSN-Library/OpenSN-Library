c58227eddbae64973462c866cc5238c74eb36a15
realssd <luwenhao_2001@outlook.com>
2023-11-22 21:10:57 +0800

    [fix]: 修复监听etcd key的常量配置

--- sub-modules/PositionProvider/global_var.py
+++ sub-modules/PositionProvider/global_var.py
@@ -12,5 +12,5 @@ MovingInstances:dict[bytes,dict[bytes,Instance]] = {}
 # etcd keys
 
 NODE_INS_INFO_KEY = "node_%d_instances"%NODE_ID # dir
-NODE_NS_LIST_KEY = "/node_%d/ins_id_list"%NODE_ID
+NODE_NS_LIST_KEY = "/node_%d/ns_list"%NODE_ID
 NODE_NS_POS_KEY_PREFIX = "/node_%d/"
