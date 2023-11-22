d772835d127879a45c528d27a9d034a955395f3f
realssd <luwenhao_2001@outlook.com>
2023-11-22 17:08:43 +0800

    [chore]: 添加服务启动脚本

--- sub-modules/PositionProvider/run.sh
+++ sub-modules/PositionProvider/run.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-export NODE_ID=0 ETCD_ADDR=10.134.148.56 ETCD_PORT=2379 && python3 main.py
+export NODE_ID=0 \
+    ETCD_ADDR=10.134.148.56 \
+    ETCD_PORT=4001 \
+    REDIS_ADDR=10.134.148.56 \
+    REDIS_PORT=6379 \
+    REDIS_PASSWORD=123456 \
+    && python3 main.py
