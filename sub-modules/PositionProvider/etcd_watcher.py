4ffef99510627ae7501a36352b59769d092a9b4b
realssd <luwenhao_2001@outlook.com>
2023-11-22 17:06:24 +0800

    [feat]: 配置位置计算模块获取节点对象变更功能

--- sub-modules/PositionProvider/etcd_watcher.py
+++ sub-modules/PositionProvider/etcd_watcher.py
@@ -1,22 +1,65 @@
-import etcd
-import time
+import etcd,redis
+import time,json
 from loguru import logger
 from global_var import *
 
-def ParseEtcdResult(res: etcd.EtcdResult):
+'''
+type Instance struct {
+    InstanceID        string
+    Name              string
+    Type              string
+    PositionChangable bool
+    ContainerID       string
+    NodeID            uint32
+    Namespace         string
+    LinksID           []string
+    Extra             map[string]string
+}
+
+Type = SATELLITE = "Satellite"
+
+Extra Has
+TLE_0 -> TLE_LINE0
+TLE_1 -> TLE_LINE1
+TLE_2 -> TLE_LINE2
+
+'''
+
+
+def ParseEtcdResult(info : dict[bytes,bytes]):
+    add_key : list[bytes] = []
+    del_key : list[bytes] = []
+    for remote_key in info.keys():
+        if remote_key not in MovingInstances.keys():
+            add_key.append(remote_key)
+    for local_key in MovingInstances.keys():
+        if local_key not in info.keys():
+            del_key.append(local_key)
     
+    for key in del_key:
+        del MovingInstances[key]
+    for key in add_key:
+        obj_map = json.loads(info[key])
+        if obj_map["type"] == ["Satellite"]:
+            inst = Instance(
+                    key.decode(),
+                    [
+                        obj_map["extra"]["TLE_0"],
+                        obj_map["extra"]["TLE_1"],
+                        obj_map["extra"]["TLE_1"]
+                    ]
+                )
+            MovingInstances[key]=inst
+        
 
 def watch_instance():
-    client = etcd.Client(host=ETCD_ADDR,port=ETCD_PORT)
-    node_info_key = "/Node_%d/Instances"
-    node_watch_key = "/Node_%d/InstanceIDList"
+    etcd_client = etcd.client(host=ETCD_ADDR,port=ETCD_PORT)
+    redis_client = redis.Redis(host=REDIS_ADDR,port=REDIS_PORT,password=REDIS_PASSWORD)
     while True:
-
         try:
-            client.watch(node_watch_key)
-            infos = client.read(node_info_key,recursive=True)
+            etcd_client.watch(NODE_INS_WATCH_KEY)
+            infos = redis_client.get(NODE_INS_INFO_KEY)
             ParseEtcdResult(infos)
-            break
         except etcd.EtcdKeyNotFound as e:
             logger.warning("Node Instance Infomation Crashed or InstanceManager Not Init, Waiting...")
             time.sleep(1)
--- sub-modules/PositionProvider/global_var.py
+++ sub-modules/PositionProvider/global_var.py
@@ -1,8 +1,16 @@
 import os
 from instance import Instance
 
-NODE_ID_STR = int(os.getenv("NODE_ID"))
+NODE_ID = int(os.getenv("NODE_ID"))
 ETCD_ADDR = os.getenv("ETCD_ADDR")
 ETCD_PORT = int(os.getenv("ETCD_PORT"))
+REDIS_ADDR = os.getenv("REDIS_ADDR")
+REDIS_PORT = int(os.getenv("REDIS_PORT"))
+REDIS_PASSWORD = int(os.getenv("REDIS_PASSWORD"))
 
-MovingInstances:dict[str,Instance] = {}
+MovingInstances:dict[bytes,Instance] = {}
+
+# etcd keys
+
+NODE_INS_INFO_KEY = "node_%d_instances"%NODE_ID # dir
+NODE_INS_WATCH_KEY = "/node_%d/ins_id_list"%NODE_ID
