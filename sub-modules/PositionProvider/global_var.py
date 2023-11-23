665f1b50b7a05f1009c4bcd00eeda25e043fd483
realssd <luwenhao_2001@outlook.com>
2023-11-23 15:22:45 +0800

    [fix]: 修改NodeIndex环境变量Key

--- sub-modules/PositionProvider/global_var.py
+++ sub-modules/PositionProvider/global_var.py
@@ -1,7 +1,7 @@
 import os
 from instance import Instance
 
-NODE_ID = int(os.getenv("NODE_ID"))
+NODE_INDEX = int(os.getenv("NODE_INDEX"))
 ETCD_ADDR = os.getenv("ETCD_ADDR")
 ETCD_PORT = int(os.getenv("ETCD_PORT"))
 REDIS_ADDR = os.getenv("REDIS_ADDR")
@@ -11,6 +11,6 @@ REDIS_PASSWORD = int(os.getenv("REDIS_PASSWORD"))
 MovingInstances:dict[bytes,dict[bytes,Instance]] = {}
 # etcd keys
 
-NODE_INS_INFO_KEY = "node_%d_instances"%NODE_ID # dir
-NODE_NS_LIST_KEY = "/node_%d/ns_list"%NODE_ID
-NODE_NS_POS_KEY_PREFIX = "/node_%d/"
+NODE_INS_INFO_KEY = "node_%d_instances"%NODE_INDEX # dir
+NODE_NS_LIST_KEY = "/node_%d/ns_list"%NODE_INDEX
+NODE_NS_POS_KEY_PREFIX = "/node_%d/"%NODE_INDEX
