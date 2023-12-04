b2699ab11be33e0c4e766270f8ea30522833be2c
realssd <luwenhao_2001@outlook.com>
2023-12-04 18:38:19 +0800

    [chore]: 添加实例管理模块和链路管理模块镜像构建配置

--- sub-modules/InstanceManager/main.go
+++ sub-modules/InstanceManager/main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import (
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+)
 
+func main() {
+	wg := new(sync.WaitGroup)
+	wg.Add(1)
+	sigChann := make(chan os.Signal,1)
+	signal.Notify(sigChann,syscall.SIGTERM)
+	go func ()  {
+		<- sigChann
+		wg.Done()	
+	}()
+	wg.Wait()
 }
--- sub-modules/LinkManager/main.go
+++ sub-modules/LinkManager/main.go
@@ -1,37 +1,20 @@
 package main
 
 import (
-	"fmt"
-
-	"github.com/vishvananda/netlink"
-	"github.com/vishvananda/netns"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
 )
 
 func main() {
-	raw,_ := netns.Get()
-	list, _ := netlink.LinkList()
-	for _, v := range list {
-		fmt.Printf("NetNs FD is %d, Interface Name is %s, IfIndex is %d\n", raw, v.Attrs().Name, v.Attrs().Index)
-	}
-	test0, _ := netns.GetFromName("test_0")
-	defer test0.Close()
-	test1, _ := netns.GetFromName("test_1")
-	defer test1.Close()
-	err := netns.Set(test0)
-	if err != nil {
-		panic(err)
-	}
-	list, _ = netlink.LinkList()
-	for _, v := range list {
-		fmt.Printf("NetNs FD is %d, Interface Name is %s, IfIndex is %d\n", test0, v.Attrs().Name, v.Attrs().Index)
-	}
-	err = netns.Set(test1)
-	if err != nil {
-		panic(err)
-	}
-	list, _ = netlink.LinkList()
-	for _, v := range list {
-		fmt.Printf("NetNs FD is %d, Interface Name is %s, IfIndex is %d\n", test1, v.Attrs().Name, v.Attrs().Index)
-	}
-
+	wg := new(sync.WaitGroup)
+	wg.Add(1)
+	sigChann := make(chan os.Signal,1)
+	signal.Notify(sigChann,syscall.SIGTERM)
+	go func ()  {
+		<- sigChann
+		wg.Done()	
+	}()
+	wg.Wait()
 }
