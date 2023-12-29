25fcbef2d5b474a180b3bde4fdcba196efe53795
realssd <luwenhao_2001@outlook.com>
2023-12-29 14:34:09 +0800

    [fix]: 修复map名称问题

--- node-daemon/bpf-src/bpf.c
+++ node-daemon/bpf-src/bpf.c
@@ -35,7 +35,7 @@ struct {
 	__uint(max_entries, 1);
 	__type(key, __u32); // source Mac Address
 	__type(value, __u32); // target interface mac address
-} ingress_map SEC(".maps");
+} gateway_map SEC(".maps");
 
 static __always_inline int parse_ethhdr(struct hdr_cursor *nh,
 					void *data_end,
@@ -47,7 +47,7 @@ static __always_inline int parse_ethhdr(struct hdr_cursor *nh,
 	/* Byte-count bounds check; check if current pointer + size of header
 	 * is after data_end.
 	 */
-	if (nh->pos + 1 > data_end)
+	if (eth + 1 > data_end)
 		return -1;
 
 	nh->pos += hdrsize;
@@ -56,10 +56,45 @@ static __always_inline int parse_ethhdr(struct hdr_cursor *nh,
 	return eth->h_proto; /* network-byte-order */
 }
 
+static __always_inline __u64 translate_mac(unsigned char* bytes) {
+	__u64 res = 0;
+	int i = 0;
+	for (i=0; i<6; i++) {
+		res <<= 8;
+		res = (__u64)(bytes[i]) | res;
+	}
+	return res;
+}
+
 SEC("xdp")
 int ingress(struct xdp_md *ctx)
 {   
-    // STEP1: parse packet, get src mac address
+    void *data_end = (void *)(long)ctx->data_end;
+	void *data = (void *)(long)ctx->data;
+	struct ethhdr *eth;
+	__u64 key = 0, value = 0;
+	/* Default action XDP_PASS, imply everything we couldn't parse, or that
+	 * we don't want to deal with, we just pass up the stack and let the
+	 * kernel deal with it.
+	 */
+	__u32 action = XDP_PASS; /* Default action */
+
+        /* These keep track of the next header type and iterator pointer */
+	struct hdr_cursor nh;
+	int nh_type;
+
+	/* Start next header cursor position at data start */
+	nh.pos = data;
+
+	nh_type = parse_ethhdr(&nh, data_end, &eth);
+
+	key = translate_mac(eth->h_source);
+	void *value_ptr = bpf_map_lookup_elem(&ingress_map,&key);
+	if (value_ptr == NULL) {
+		return XDP_DROP;
+	}
+	
+	value = (__u64*)
     // STEP1: lookup ingress map, get redirect ifindex
     // STEP2: redirect
 }
