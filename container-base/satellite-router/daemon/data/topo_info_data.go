17c696ce287fe7af53bcb0d25d1344b39e87e7c4
realssd <luwenhao_2001@outlook.com>
2024-01-05 08:23:22 +0000

    [feat]: 区分arm64架构版本镜像(for arm macbook vm)

--- container-base/satellite-router/daemon/data/topo_info_data.go
+++ container-base/satellite-router/daemon/data/topo_info_data.go
@@ -5,6 +5,7 @@ import (
 	"os"
 	"satellite/config"
 	"satellite/model"
+	"time"
 
 	"github.com/sirupsen/logrus"
 )
@@ -17,13 +18,15 @@ func init() {
 		fd, err := os.Open(config.TopoInfoPath)
 		if err != nil {
 			logrus.Errorf("Open Topo Infomation File Error: %s", err.Error())
-			panic(err)
+			time.Sleep(time.Second)
+			continue
 		}
 		decoder := json.NewDecoder(fd)
 		err = decoder.Decode(&TopoInfo)
 		if err != nil {
 			logrus.Errorf("Decode Json Topo Infomation Error: %s", err.Error())
-			panic(err)
+			time.Sleep(time.Second)
+			continue
 		}
 		success = true
 	}
